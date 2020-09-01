f9f56fa8fa7205e41796c3402191b43436b01943
Dmitry Gordeev <dmitrijg934@gmail.com>
2020-09-01 16:30:01 +0300

    Initial commit. Cleaned-up repo

---
+++ src/main/java/nn/dgordeev/workchat/WorkchatApplication.java
@@ -0,0 +1,13 @@
+package nn.dgordeev.workchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkchatApplication.class, args);
+    }
+
+}
